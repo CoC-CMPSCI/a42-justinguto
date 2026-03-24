8da89a576d77a48f24ae9b903dad269c816c255a
justinguto <163184050+justinguto@users.noreply.github.com>
2026-03-23 17:25:55 -0700

    Implement discount logic for sold units
    Added discount calculation based on units sold and adjusted output for total cost.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,56 @@
-
-//?  [Problem Definition]
-//*     Write a program that asks for the weight of the package and
-//*     the distance it is to be shipped, and then displays the charges.
-//*
-//*  The Fast Freight Shipping Company charges the following rates:
-//*     Weight of Package (in Kilograms)          Rate per 500 Miles Shipped
-//*        2 kg or less                              $1.10
-//*        Over 2 kg but not more than 6 kg          $2.20
-//*        Over 6 kg but not more than 10 kg         $3.70
-//*        Over 10 kg but not more than 20 kg        $4.80
-//*
-//** Input Validation :
-//* 	Do not accept values of 0 or less for the weight of the package.
-//* 	Do not accept weights of more than 20 kg (this is the maximum weight the company will ship).
-//* 	Do not accept distances of less than 10 miles or more than 3,000 miles.
-//*     These are the company's minimum and maximum shipping distances.
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
-    const double RATE2  = 1.10;
-    const double RATE6  = 2.20;
-    const double RATE10 = 3.70;
-    const double RATE20 = 4.80;
-    double weight, distance, dRate, price;
+    const double PERC19 = 0.2;
+    const double PERC49 = 0.3;
+    const double PERC99 = 0.4;
+    const double PERC100 = 0.5;
+    const double Price = 99.00;
 
-    cout << "Enter the package weight and distance\n";
-    cin >> weight >> distance;
+    double totalCost, originalAmount, discountAmount, dRate;
+    int numberofSold;
 
-    // TODO: validate input - weight must be > 0 and <= 20
+    cout << "Enter the number of units sold\n";
+    cin >> numberofSold;
+    
+    if (numberofSold < 0)
+    {
+        cout << "Error: input must be a positive integer." << endl;
+        return 0;
+    }
 
-    // TODO: validate input - distance must be >= 10 and <= 3000
+    if (numberofSold >= 100)
+    {
+        dRate = PERC100;
+    }
+    else if (numberofSold >= 50)
+    {
+        dRate = PERC99;
+    }
+    else if (numberofSold >= 20)
+    {
+        dRate = PERC49;
+    }
+    else if (numberofSold >= 10)
+    {
+        dRate = PERC19;
+    }
+    else
+    {
+        dRate = 0.0;
+    }
 
-    // TODO: determine rate based on weight
+    originalAmount = numberofSold * Price;
+    discountAmount = originalAmount * dRate;
+    totalCost = originalAmount - discountAmount;
 
-    // TODO: calculate price based on distance
-
-    // Use the following statements to print output
     cout << setprecision(2) << fixed;
-    cout << "The shipping price for package is " << price << endl;
+    cout << "Original amount is " << originalAmount << endl;
+    cout << "Discount amount is " << discountAmount << endl;
+    cout << "Total Price is " << totalCost << endl;
 
     return 0;
 }
